80d35b6a308f90fbaf4cd3aba01c11ac128f393b
dcoronag <dcoronag@P1C030.lacartuja.ind.local>
2018-05-09 20:30:54 +0200

    update

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,3 +5,27 @@ public class HolaMundo {
 		System.out.println("Lección 9 del curso https://www.tutorialesprogramacionya.com/javaya/ ");
 	}
 }
+
+/**
+ * T1.- Java
+ * T2.- Eclipse
+ * T3.- HolaMundo
+ * T4.- SueldoOperario
+ * T5.- SuperficieCuadrado
+ * T6.- SumaProductoNumeros , PerimetroCuadrado , SumaProducto4Numeros , SumaPromedio , CostoCompra 
+ * T7.- EstructuraCondicionalSimple1 , EstructuraCondicionalCompuesta1 , EstructuraCondicionalCompuesta2 , EstructuraCondicionalSimple2 , EstructuraCondicionalCompuesta3
+ * T8.- EstructuraCondicionalAnidada1 , EstructuraCondicionalAnidada2 , EstructuraCondicionalAnidada3 , EstructuraCondicionalAnidada4 , EstructuraCondicionalAnidada5  
+ * T9.- CondicionesCompuestas1 , CondicionesCompuestas2 , CondicionesCompuestas3 , CondicionesCompuestas4 , CondicionesCompuestas5, CondicionesCompuestas6 , CondicionesCompuestas7 , CondicionesCompuestas8 , CondicionesCompuestas9 
+ * T10.-
+ * T11.-
+ * T12.-
+ * T13.-
+ * T14.-
+ * T15.-
+ * T16.-
+ * T17.-
+ * T18.-
+ * T19.-
+ * T20.-
+ *  
+ */
