02d77d671083e936fa4db5d3512edfecd2bb3e17
dcoronag <dcoronag@P1C029.lacartuja.ind.local>
2018-07-27 22:44:13 +0200

    update

--- src/Formulario.java
+++ src/Formulario.java
@@ -1,22 +1,46 @@
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Formulario extends JFrame {
-    private JLabel label1,label2;
-    
+public class Formulario extends JFrame implements ActionListener {
+    private JButton boton1, boton2, boton3, boton4;
     public Formulario() {
         setLayout(null);
-        label1=new JLabel("Sistema de gestión de nómina.");
-        label1.setBounds(10,20,300,30);
-        add(label1);
-        label2=new JLabel("Vesion 1.0");
-        label2.setBounds(10,200,100,30);
-        add(label2);
+        boton1=new JButton("Finalizar");
+        boton1.setBounds(300,250,100,30);
+        add(boton1);
+        boton1.addActionListener(this);
+        boton2=new JButton("2");
+        boton2.setBounds(10,100,90,30);
+        add(boton2);
+        boton2.addActionListener(this);
+        boton3=new JButton("3");
+        boton3.setBounds(110,100,90,30);
+        add(boton3);
+        boton3.addActionListener(this);
+        boton4=new JButton("4");
+        boton4.setBounds(210,100,90,30);
+        add(boton4);
+        boton4.addActionListener(this);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==boton1) {
+            System.exit(0);
+        }
+        if (e.getSource()==boton2) {
+            setTitle("Boton 2");
+        }
+        if (e.getSource()==boton3) {
+            setTitle("Boton 3");
+        }
+        if (e.getSource()==boton4) {
+            setTitle("Boton 4");
+        }    
     }
     
     public static void main(String[] ar) {
         Formulario formulario1=new Formulario();
-        formulario1.setBounds(0,0,300,500);
-        formulario1.setResizable(false);
+        formulario1.setBounds(0,0,450,350);
         formulario1.setVisible(true);
     }
 }
